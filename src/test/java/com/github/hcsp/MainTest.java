b211da141ae60bf3c6ae772d4b8ffc283e9bab36
Bo Zhang <bo@gradle.com>
2019-06-08 17:53:45 +0800

    Init repo

---
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -0,0 +1,17 @@
+package com.github.hcsp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void test() {
+        java.util.Random r = new java.util.Random();
+        int[][] m = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        m[0][0] = r.nextInt(100);
+        m[1][1] = r.nextInt(100);
+        m[2][2] = r.nextInt(100);
+        assertEquals(m[0][0] + m[1][1] + m[2][2], Main.diagonalSum(m));
+    }
+}
