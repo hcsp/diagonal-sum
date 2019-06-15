5fc96c1e278bc639340cd2c4f52b00964d2ce53c
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,17 +1,17 @@
 package com.github.hcsp.controlflow;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
-        java.util.Random r = new java.util.Random();
+        Random r = new Random();
         int[][] m = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         m[0][0] = r.nextInt(100);
         m[1][1] = r.nextInt(100);
         m[2][2] = r.nextInt(100);
-        assertEquals(m[0][0] + m[1][1] + m[2][2], Main.diagonalSum(m));
+        Assertions.assertEquals(m[0][0] + m[1][1] + m[2][2], Main.diagonalSum(m));
     }
 }
