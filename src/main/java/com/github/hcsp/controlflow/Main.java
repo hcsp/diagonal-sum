594d18a089c4a31bf9d2551c375fc7d934ba9d4e
lambert1129 <lambert1129@foxmail.com>
2019-06-18 15:31:34 +0800

    y

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {{10, 2, 3,1}, {4, 5, 6,1}, {7, 8, 9,1}, {7, 8, 9,1}};
+        int[][] matrix = {{10, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println(diagonalSum(matrix));
     }
 
