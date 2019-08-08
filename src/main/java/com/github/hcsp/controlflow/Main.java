fb902553938fbcde4ca0ecd2c4f1260144942f8d
silvia-YQY <545067444@qq.com>
2019-08-08 15:33:52 +0800

    matrix[0][0] + matrix[length-1][length-1]; (#41)
    matrix[0][0] + matrix[length-1][length-1]; (#41)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int  sum=0;
+        int length = matrix.length;
+        for (int i = 0; i <length ; i++) {
+            sum+=matrix[i][i];
+
+        }
+        return sum;
+    }
 }
