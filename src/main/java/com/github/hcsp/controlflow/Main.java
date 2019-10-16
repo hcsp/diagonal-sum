da83f7608b231f50b7aeb4db815c6fb0f1436d5a
dqprecisian <36305085+dqprecisian@users.noreply.github.com>
2019-10-16 07:52:49 -0500

    求数组对角元素之和 (#77)
    求数组对角元素之和 (#77)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int sum = 0;
+        for(int i = 0; i < matrix.length; i++) {
+
+            sum += matrix[i][i];
+
+        }
+        return sum;
+    }
 }
