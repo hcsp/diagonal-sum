b03cf466e7512fd891babc6a72707b44f173563d
richard1230 <huyongjinnuaa@163.com>
2020-01-03 23:55:58 +0800

    对角线之和

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int res = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            res += matrix[i][i];
+        }
+        return res;
+
+
+    }
 }
