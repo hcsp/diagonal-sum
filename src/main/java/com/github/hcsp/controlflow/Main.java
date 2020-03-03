53aae7a4f3d349e8533c908ed3cde98ef15bfb19
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-03 11:02:41 +0800

    diagonalSum (#143)
    diagonalSum (#143)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,17 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int sum = 0;
+        int weiShu = 0;
+        for (int[] ints : matrix) {
+            weiShu++;
+        }
+
+        for (int i = 0; i < weiShu; i++) {
+            sum += matrix[i][i];
+        }
+
+        return sum;
+    }
 }
