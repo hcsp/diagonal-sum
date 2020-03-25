aac89aed6bbb4f64a6f46cea20edea77d147499f
Huang xuewei <770342643@qq.com>
2020-03-26 05:25:51 +0800

    add solution codes in diagonalSum method (#160)
    add solution codes in diagonalSum method (#160)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int sum = 0;
+
+        for (int row = 0; row < matrix.length; row++) {
+              sum += matrix[row][row];
+        }
+
+        return sum;
+    }
 }
