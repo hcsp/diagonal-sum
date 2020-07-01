12d31dbbabbc05845a45fe39aad2002d01a50b5b
2014 <suhanma@gmail.com>
2020-07-01 09:06:59 -0700

    sum (#194)
    sum (#194)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        while (i < matrix.length && j < matrix.length) {
+            sum += matrix[i++][j++];
+        }
+        return sum;
+    }
 }
