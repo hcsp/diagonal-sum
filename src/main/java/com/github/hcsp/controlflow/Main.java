f04f76b0d6fe88866e43d899e8ec6c1a629848ec
weiranyi <67884344+weiranyi@users.noreply.github.com>
2020-11-29 22:40:16 +0800

    规范了代码格式 (#209)
    规范了代码格式 (#209)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,7 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        return matrix[0][0] + matrix[1][1] + matrix[2][2];
+    }
 }
