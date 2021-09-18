24a93ac763c201a6daf2abf6bb6e4bd392120c23
Xionghao Zhao <86486489+GrumpyCitizenBear@users.noreply.github.com>
2021-09-18 12:22:19 +0800

    求NxN二维数组对角线元素之和 (#230)
    求NxN二维数组对角线元素之和 (#230)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,19 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+
+        int sum = 0;
+
+        for(int i = 0;i < matrix.length;i++){
+            for(int j = 0;j<matrix.length;j++){
+
+                if(i==j){
+                    sum += matrix[i][j];
+                }
+
+            }
+        }
+        return sum;
+    }
 }
