2a313177e8f6a21989aab5f2e5cce8790f8d9df6
LiDianguang <101635266+LiDianguang@users.noreply.github.com>
2022-03-21 22:01:54 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,11 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            count += matrix[i][i];
+        }
+        return count;
+    }
 }
