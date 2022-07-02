e5619d24ba4fda2acc88a373a826719b5ee3367b
XIANG CHEN <58834456+futrue1024@users.noreply.github.com>
2022-07-02 17:12:16 +0800

    int [][] (#258)
    int [][] (#258)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,11 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int a = 0;
+        for (int m = 0; m < matrix.length; m++) {
+            a += matrix[m][m];
+        }
+        return a;
+    }
 }
