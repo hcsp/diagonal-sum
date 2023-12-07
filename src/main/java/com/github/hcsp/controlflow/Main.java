f768e8198d8d38c1eca7a20e7c1b01857dd9a909
Leo Cheung <leozhang621@gmail.com>
2023-12-07 15:25:29 +0800

    feat: completed missions

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,12 @@ public class Main {
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+        int result = 0;
+        for (int i = 0; i < matrix.length ; i++) {
+            int[] element = matrix[i];
+            result += element[i];
+        }
+        return result;
+    }
 }
