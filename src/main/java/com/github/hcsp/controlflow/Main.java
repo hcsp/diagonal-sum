fbe5b04c60ac7438e3e239b047b89ab326ce3f40
GJV123456 <161851858+GJV123456@users.noreply.github.com>
2024-09-10 17:04:47 +0800

    矩阵对角元素求和 (#269)
    矩阵对角元素求和 (#269)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,10 +7,22 @@ public class Main {
     }
 
     /**
-     * 给定一个二维数组，返回其主对角线（从左上到右下）元素之和。你可以假设该数组是NxN的，即一维和二维长度相同，都是N。
+     * 给定一个二维数组，返回其主对角线（从左上到右下）元素之和。
+     * 你可以假设该数组是NxN的，即一维和二维长度相同，都是N。
      *
      * @param matrix 给定的二维数组
      * @return 对角线元素之和
      */
-    public static int diagonalSum(int[][] matrix) {}
+    public static int diagonalSum(int[][] matrix) {
+//        System.out.println(matrix.length);
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
 }
